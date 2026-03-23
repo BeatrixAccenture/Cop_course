dbed42bb7ded9d6548a175074a466b1ceb3ab38d
BeatrixAccenture <beatrix.berdan@accenture.com>
2026-03-23 13:12:50 +0000

    Fix: Remove invalid @api_root assignment in urls.py

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -3,18 +3,27 @@ from django.urls import path, include
 
 from rest_framework import routers
 from . import views
+
+import os
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 
+
+@api_root = os.environ.get('CODESPACE_NAME')
 @api_view(['GET'])
 def api_root(request, format=None):
+    codespace_name = os.environ.get('CODESPACE_NAME')
+    if codespace_name:
+        base_url = f"https://{codespace_name}-8000.app.github.dev/api"
+    else:
+        base_url = "http://localhost:8000/api"
     return Response({
-        'users': '/users/',
-        'teams': '/teams/',
-        'activities': '/activities/',
-        'leaderboard': '/leaderboard/',
-        'workouts': '/workouts/',
+        'users': f'{base_url}/users/',
+        'teams': f'{base_url}/teams/',
+        'activities': f'{base_url}/activities/',
+        'leaderboard': f'{base_url}/leaderboard/',
+        'workouts': f'{base_url}/workouts/',
     })
 
 router = routers.DefaultRouter()
